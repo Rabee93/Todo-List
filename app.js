735596db228b0e288da248df5b701235ccf30726
Rabee Mghrabi <learningprogramming2019@gmail.com>
2020-11-01 19:58:10 +0000

    fixed bug line 5 app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const todoInput = document.querySelector('.todo-input')
 const todoButton = document.querySelector('.todo-button')
 const todoList = document.querySelector('.todo-list')
 
-todoButton.addeventListener('click',addTodo);
+todoButton.addEventListener('click',addTodo);
 
 function addTodo(event){
   event.preventDefault();
