cf6eb07c832368d802a60fcb5ff36179e9bab3ac
Rabee Mghrabi <learningprogramming2019@gmail.com>
2020-11-04 20:37:03 +0000

    fixed bug of filter-todo

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const todoInput = document.querySelector('.todo-input');
 const todoButton = document.querySelector('.todo-button');
 const todoList = document.querySelector('.todo-list');
-const filterOption = document.querySelector('.filterTodo')
+const filterOption = document.querySelector('.filter-todo')
 
 todoButton.addEventListener('click',addTodo);
 todoList.addEventListener('click', deleteCheck);
