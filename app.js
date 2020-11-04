fca6042568accc0b6a23508417556f9e44f44732
Rabee Mghrabi <learningprogramming2019@gmail.com>
2020-11-04 20:30:45 +0000

    fixed transition event listener bug

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const todoInput = document.querySelector('.todo-input');
 const todoButton = document.querySelector('.todo-button');
 const todoList = document.querySelector('.todo-list');
+const filterOption = document.querySelector('.filterTodo')
 
 todoButton.addEventListener('click',addTodo);
 todoList.addEventListener('click', deleteCheck);
+filterOption.addEventListener('click', filterTodo)
 
 function addTodo(event){
   event.preventDefault();
@@ -36,7 +38,8 @@ function deleteCheck(e) {
   if (item.classList[0] === "trash-btn") {
     const todo = item.parentElement;
     todo.classList.add("fall");
-    todo.addEventListener('transitioned', function(){
+    todo.addEventListener('transitionend', function(){
+console.log("hi")
       todo.remove();
     });
   }
@@ -44,4 +47,21 @@ function deleteCheck(e) {
     const todo = item.parentElement;
     todo.classList.toggle("completed");
   }
+}
+
+function filterTodo(e){
+const todos = todoList.childNodes;
+todos.forEach(function(todo){
+  switch(e.target.value){
+    case"all":
+    todo.style.display = "flex";
+    break;
+    case "completed":
+    if(todo.classList.contains('completed')){
+      todo.style.display = 'flex';
+    }else {
+      todo.style.display = "none";
+    }
+  }
+})
 }
